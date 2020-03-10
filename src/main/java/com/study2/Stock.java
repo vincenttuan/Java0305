9416d8a5db947d281ad09f206c52977815fd984a
MB-teacher <MB-teacher@192.168.1.109>
2020-03-10 21:32:31 +0800

    Java

--- src/main/java/com/study2/Stock.java
+++ src/main/java/com/study2/Stock.java
@@ -5,6 +5,7 @@ package com.study2;
 * 版本 : 1.0                                *
 * 日期 : 2020/03/10                         *
 ********************************************/
+import java.util.Scanner;
 public class Stock {
     /** 這是程式進入點 */
     public static void main(String[] args) {
@@ -12,8 +13,10 @@ public class Stock {
         String stockName = "台積電";
         // 股票價格(股)
         double stockPrice = 307.5;
-        // 買進股數
-        int amount = 3500;
+        // 買進股數(由使用者來輸入)
+        System.out.println("請輸入買進股數(例如:1000) = ");
+        Scanner sc = new Scanner(System.in); // System.in 預設是鍵盤
+        int amount = sc.nextInt(); // 取出鍵盤key in的資料並轉成 int
         // 買進成本
         double cost = stockPrice * amount;
         // 將資料印出
--- src/main/java/com/study2/Stock2.java
+++ src/main/java/com/study2/Stock2.java
@@ -13,5 +13,18 @@ public class Stock2 {
         int love = '愛';
         int you = '你';
         System.out.println(i + love + you);
+        //------------------------------
+        System.out.println(0xab);
+        //------------------------------
+        double x = 42000000; // 4.2E7
+        System.out.println(x);
+        //------------------------------
+        System.out.println("\u1F601");
+        //------------------------------
+        // 轉型
+        byte b = (byte)257;
+        System.out.println("b:\t" + b);
+        byte b2 = (byte)150;
+        System.out.println("b2:\t" + b2);
     }
 }
