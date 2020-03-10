984c744cf45a6ded56f88d2ec482e0be5aa96de4
MB-teacher <MB-teacher@192.168.1.109>
2020-03-10 20:00:40 +0800

    Java

--- src/main/java/com/study2/Stock.java
+++ src/main/java/com/study2/Stock.java
@@ -1,17 +1,24 @@
 package com.study2;
-
+/********************************************
+* 股票成本計算程式                            *
+* 作者 : Vincent                            *
+* 版本 : 1.0                                *
+* 日期 : 2020/03/10                         *
+********************************************/
 public class Stock {
+    /** 這是程式進入點 */
     public static void main(String[] args) {
         // 股票名稱
         String stockName = "台積電";
         // 股票價格(股)
         double stockPrice = 307.5;
         // 買進股數
-        int amount = 3000;
+        int amount = 3500;
         // 買進成本
         double cost = stockPrice * amount;
         // 將資料印出
-        System.out.printf("股票名稱: %s 買進成本: %,.1f", stockName, cost);
+        System.out.printf("股票名稱: %s 買進成本: %,.1f\n", stockName, cost);
+        System.out.printf("股票名稱: %s 買進成本: %,.1f\n", stockName, cost);
         
         
     }
---
+++ src/main/java/com/study2/Stock2.java
@@ -0,0 +1,17 @@
+package com.study2;
+
+public class Stock2 {
+    public static void main(String[] args) {
+        System.out.println(0b10111);
+        char c = 'A'; // ASCII = 65
+        int code = '段';
+        char c2 = 27573;
+        System.out.println(code);
+        System.out.println(c2);
+        //------------------------------
+        int i = '我';
+        int love = '愛';
+        int you = '你';
+        System.out.println(i + love + you);
+    }
+}
