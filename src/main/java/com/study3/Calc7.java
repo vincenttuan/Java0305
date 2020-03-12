dbd4a5254656e92e5a16330a1192fe92b4afb7d5
MB-teacher <MB-teacher@192.168.1.109>
2020-03-12 21:43:01 +0800

    Java

---
+++ src/main/java/com/study3/Calc7.java
@@ -0,0 +1,12 @@
+package com.study3;
+
+public class Calc7 {
+    public static void main(String[] args) {
+        int x = 5 >>> 1;
+        System.out.println(x);
+        int a = 10;
+        int b = 20;
+        a *= b; // a = a * b
+        System.out.println(a);
+    }
+}
