0da1c7e3f247e3928eb65f3a5122e5780ddc64cd
MB-teacher <MB-teacher@192.168.1.109>
2020-03-17 19:39:52 +0800

    Java

--- src/main/java/com/study4/InputBMIDemo.java
+++ src/main/java/com/study4/InputBMIDemo.java
@@ -10,6 +10,15 @@ public class InputBMIDemo {
         System.out.println("請輸入體重(kg): ");
         double w = sc.nextDouble();
         double bmi = w / Math.pow(h/100, 2);
-        System.out.printf("身高: %.1f 體重: %.1f BMI: %.1f\n", h, w, bmi);
+        // 判定結果
+        String result = "";
+        if(bmi > 23)
+            result = "過重";
+        else if(bmi <= 18) {
+            result = "過輕";
+        } else {
+            result = "正常";
+        }
+        System.out.printf("身高: %.1f 體重: %.1f BMI: %.1f 判定結果: %s\n", h, w, bmi, result);
     }
 }
