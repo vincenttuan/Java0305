1aed15a78f0dd5bc6981b10442d271c16cc0f8c1
MB-teacher <MB-teacher@192.168.1.109>
2020-03-17 21:33:45 +0800

    Java

--- src/main/java/com/study4/InputLevelDemo.java
+++ src/main/java/com/study4/InputLevelDemo.java
@@ -6,7 +6,20 @@ public class InputLevelDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("請輸入 Level (A - H): ");
-        
+        String level = sc.next();
+        switch(level) {
+            case "A":
+            case "B":
+                System.out.println("高標");
+                break;
+            case "C":
+            case "D":
+            case "E":
+                System.out.println("中標");
+                break;
+            default:
+                System.out.println("低標");
+        }
         
     }
 }
