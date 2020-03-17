76b217b057008f8b98b503a13cca49ad5b95beb9
MB-teacher <MB-teacher@192.168.1.109>
2020-03-17 21:22:21 +0800

    Java

---
+++ src/main/java/com/study4/InputLevelDemo.java
@@ -0,0 +1,12 @@
+package com.study4;
+
+import java.util.Scanner;
+
+public class InputLevelDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("請輸入 Level (A - H): ");
+        
+        
+    }
+}
