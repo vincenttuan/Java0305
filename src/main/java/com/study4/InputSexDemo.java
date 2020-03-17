c91288ee160921c7d7cb5a2e900e2f744c101046
MB-teacher <MB-teacher@192.168.1.109>
2020-03-17 21:45:42 +0800

    Java

--- src/main/java/com/study4/InputSexDemo.java
+++ src/main/java/com/study4/InputSexDemo.java
@@ -3,20 +3,21 @@ package com.study4;
 import java.util.Scanner;
 
 public class InputSexDemo {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("請輸入身分證字號: ");
         String id = sc.next();
         char sex = id.charAt(1);
-        if(sex == '1') {
+        if (sex == '1') {
             System.out.println("男生");
-        } else if(sex == '2') {
+        } else if (sex == '2') {
             System.out.println("女生");
         } else {
             System.out.println("錯誤");
         }
         // 使用 switch-case
-        switch(sex) {
+        switch (sex) {
             case '1': // ASCII = 49
                 System.out.println("男生");
                 break;
@@ -25,7 +26,7 @@ public class InputSexDemo {
                 break;
             default:
                 System.out.println("錯誤");
-                //break;
-        }
+            //break;
+            }
     }
 }
