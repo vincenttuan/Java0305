427b87f03be14c945c885cf47f9701552849b43f
MB-teacher <MB-teacher@192.168.1.109>
2020-03-19 19:25:52 +0800

    Java

---
+++ src/main/java/com/study5/NineTable.java
@@ -0,0 +1,15 @@
+package com.study5;
+
+public class NineTable {
+    public static void main(String[] args) {
+        
+        int x = 1;
+        int y = 1;
+        while(y <= 9) {
+            int z = x * y;
+            System.out.printf("%d * %d = %d\t", x, y, z);
+            y++;
+        }
+        
+    }
+}
