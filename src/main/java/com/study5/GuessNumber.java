771bc4aa859ca35eab1bccd0eae6f5cbb6b3e67a
MB-teacher <MB-teacher@192.168.1.109>
2020-03-19 19:59:16 +0800

    Java

--- src/main/java/com/study5/GuessNumber.java
+++ src/main/java/com/study5/GuessNumber.java
@@ -9,12 +9,16 @@ public class GuessNumber {
         int min = 0;
         int max = 100;
         do {            
+            // 1.請使用者輸入
             Scanner sc = new Scanner(System.in);
             System.out.printf("請猜一個數字 %d ~ %d :\n", min, max);
             int guess = sc.nextInt();
-            // 檢查 guess 是否在 min 與 max 之間 ?
-            
-            
+            // 2.檢查 guess 是否在 min 與 max 之間 ?
+            if(!(guess > min && guess < max)) { // guess <= min || guess >= max
+                System.out.println("資料範圍錯誤, 請重新輸入");
+                continue;
+            }
+            // 3.判斷是否答對 ?
             if(guess > ans) {
                 max = guess;
                 continue;
