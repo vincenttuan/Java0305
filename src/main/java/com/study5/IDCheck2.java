4d07f95fdeeb00bb2c890738d3172c17a1d88097
MB-teacher <MB-teacher@192.168.1.109>
2020-03-19 21:35:02 +0800

    Java

--- src/main/java/com/study5/IDCheck2.java
+++ src/main/java/com/study5/IDCheck2.java
@@ -5,13 +5,12 @@ import java.util.Random;
 public class IDCheck2 {
 
     public static void main(String[] args) {
-        int sum;
         do {
-            sum = 0;
             Random r = new Random();
             // 找出桃園一組合法身分證字號
             int[] nums = {1, 7, r.nextInt(2) + 1, r.nextInt(10), r.nextInt(10), r.nextInt(10), r.nextInt(10), r.nextInt(10), r.nextInt(10), r.nextInt(10), r.nextInt(10)};
             int[] mult = {1, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1};
+            int sum = 0;
             for (int i = 0; i < 11; i++) {
                 int sub = nums[i] * mult[i];
                 sum += sub;
@@ -22,7 +21,10 @@ public class IDCheck2 {
                 System.out.print(nums[i]);
             }
             System.out.println(sum % 10 == 0 ? "合法" : "不合法");
-        } while (sum % 10 != 0);
+            if(sum % 10 == 0) {
+                break;
+            }
+        } while (true);
 
     }
 }
