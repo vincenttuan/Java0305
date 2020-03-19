49f84ff423d242e7286435504015d5e5b0bcd9b3
MB-teacher <MB-teacher@192.168.1.109>
2020-03-19 19:29:18 +0800

    Java

--- src/main/java/com/study5/NineTable.java
+++ src/main/java/com/study5/NineTable.java
@@ -4,12 +4,18 @@ public class NineTable {
     public static void main(String[] args) {
         
         int x = 1;
-        int y = 1;
-        while(y <= 9) {
-            int z = x * y;
-            System.out.printf("%d * %d = %d\t", x, y, z);
-            y++;
+        while (x <= 9) {            
+            int y = 1;
+            while (y <= 9) {
+                int z = x * y;
+                System.out.printf("%d * %d = %d\t", x, y, z);
+                y++;
+            }
+            System.out.println();
+            x++;
         }
+        
+        
         
     }
 }
